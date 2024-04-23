c4942e0b1af3113729329457ba1e484179bd16da
JADSN <120682244+JADSN1894@users.noreply.github.com>
2024-04-23 19:47:38 +0000

    feat: Attempt

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
 
 import { instantiate } from "./lib/rs_lib.generated.js";
 
-export async function sum(a: number, b: number) {
+export async function sum(a: number, b: number): Promise<number> {
 	const { add } = await instantiate();
-	console.log(add(a, b));
+	return add(a, b);
 }
