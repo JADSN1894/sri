09b965928e416926a881a3536ee4dd39f81d1c9c
JADSN <120682244+JADSN1894@users.noreply.github.com>
2024-04-23 19:22:17 +0000

    feat: Attempt

--- main.ts
+++ main.ts
@@ -1,60 +1,11 @@
-//* Executes after build
-//* 1. Export the function to the package
-//* 2. Read the content .js and .css files at dist project folder
-//* 3. Obtain the sha-[256 | 512] of the files
-//* 4. Update the scripts and links of the index.html
+// import { instantiate } from "./lib/rs_lib.generated.js";
 
-//* Arguments
-//* 1. Airtifact folder
-//* 2. Encoding algorithm
+// const { add } = await instantiate();
+// console.log(add(1, 1));
 
-import { WASI } from "node:wasi";
-import { env } from "node:process";
-import { readFile } from "node:fs/promises";
-export type Algorithm = "Sha256" | "Sha384" | "Sha512";
+import { instantiate } from "./lib/rs_lib.generated.js";
 
-export interface SriOptions {
-	/**
-	 * Which hashing algorithms to use when calculate the integrity hash for each
-	 * asset in the manifest.
-	 *
-	 * @default 'Sha512'
-	 */
-	algorithm: Algorithm;
-
-	/**
-	 * Airtifact path
-	 *
-	 * @default 'Sha512'
-	 */
-	outDir: string;
+export async function sum(a: number, b: number) {
+	const { add } = await instantiate();
+	console.log(add(a, b));
 }
-
-function subresourceIntegrity(
-	options: SriOptions = {
-		algorithm: "Sha512",
-		outDir: "dist",
-	},
-): void {
-	const { algorithm, outDir } = options;
-	const wasi = new WASI({
-		version: "preview1",
-		args: ["sri", algorithm, outDir],
-		env,
-		preopens: {
-			"/": "/",
-			".": ".",
-		},
-	});
-
-	(async () => {
-		const wasm = await WebAssembly.compile(await readFile("sri.wasm"));
-
-		const instance = new WebAssembly.Instance(wasm, {
-			wasi_snapshot_preview1: wasi.wasiImport,
-		});
-		wasi.start(instance);
-	})();
-}
-
-// subresourceIntegrity();
